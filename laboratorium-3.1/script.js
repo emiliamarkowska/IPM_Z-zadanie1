215f7cd906dd05101a8975e513b0215c62377e8d
emiliamarkowska <emarkowska10@gmail.com>
2022-01-15 11:33:36 +0100

    moving blocks implemented

---
+++ laboratorium-3.1/script.js
@@ -0,0 +1,86 @@
+var randomColor;
+var currentId = 0;
+var squareSize = 50;
+
+function setBlockColor() {
+    randomColor = Math.floor(Math.random()*16777215).toString(16);
+    block = document.getElementById("block");
+    block.style.backgroundColor = randomColor;
+}
+
+function onDragStart(ev) {
+}
+  
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function onDragEnd(ev) {
+      console.log(ev)
+      console.log("drag end")
+        document.getElementById(ev.srcElement.id).remove();
+  }
+
+  function dragElementStarted(ev) {
+      console.log(ev);
+      console.log("drag started");
+  }
+
+function handleDrop(ev) {
+    ev.preventDefault();
+    currentBlock = document.getElementById("block")
+    newBlock = document.createElement("div");
+    newBlock.setAttribute("draggable", true);
+    newBlock.setAttribute("ondragend", "onDragEnd(event)");
+    newBlock.setAttribute("ondragstart", "dragElementStarted(event)");
+    // newBlock.setAttribute("ondragstart", "onDragStart(event)");
+
+    setNewBlockInitialStyle(newBlock);
+    currentId = currentId + 1;
+
+    newBlock.style.position = "absolute";
+    newBlock.style.bottom = 0;
+    newBlock.style.left = ev.offsetX;
+
+    if(platform.children.length > 0) {
+        putBlock(newBlock, platform.children, ev)
+    }
+
+
+    platform.appendChild(newBlock);
+}
+
+function setNewBlockInitialStyle(newBlock) {
+    newBlock.id = currentId;
+    newBlock.style.backgroundColor = randomColor;
+    newBlock.style.height = squareSize;
+    newBlock.style.width = squareSize;
+}
+
+function putBlock(newBlock, allBlocks, event) {
+    xCoordinate = newBlock.offsetLeft;
+    console.log(allBlocks)
+    for(let i = 0; i < allBlocks.length; i++) {
+        currentlyCheckedBlock = allBlocks[i];
+        currentBoxLeftOffset = currentlyCheckedBlock.offsetLeft;
+
+        let potentialHeightToBeSet = 0;
+
+        if(isSomethingBelowTheBlock(currentBoxLeftOffset, event.offsetX, squareSize)) {
+            newBlock.style.left = event.offsetX;
+            potentialHeightToBeSet = parseInt(currentlyCheckedBlock.style.bottom) + squareSize;
+        }
+
+        newBlock.style.bottom = potentialHeightToBeSet;
+    }
+}
+
+function isSomethingBelowTheBlock(currentBoxLeftOffset, eventOffset, squareSize) {
+    return currentBoxLeftOffset < eventOffset + squareSize && currentBoxLeftOffset + squareSize > eventOffset
+}
+
+  
+function handleDragOver(ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+}
